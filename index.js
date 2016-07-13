387fa8547150b90baee8e62ee3d4dc44893ff163
bhupendrasingh <bhupendrasingh.2@gmail.com>
2016-07-13 14:49:53 -0400

    fixed the chat.js path

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 var express = require("express");
 var app = express();
 app.set('views', __dirname + '/tpl');
-app.set('view-engine', "jade");
-app.engine('jade', require('jade').__express);
+app.set('view engine', "jade");
+app.engine("jade", require("jade").__express);
 app.get("/",function(req,res){
    // res.send("Hello world");
     res.render("page");
 });
+app.use(express.static(__dirname + '/public'));
+var io = require('socket.io').listen(app.listen(8000));
 
-app.listen(8000);
+io.sockets.on('connection', function(socket){
+    socket.emit('message', {message: 'welcome to the chat'});
+    socket.on('send', function(data){
+        io.sockets.emit('message',data);
+    });
+});
 console.log("App running on port 8000");
